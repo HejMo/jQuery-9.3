63a4dbc4ef399c3429973b4f1c5eda59fb4d3fe9
Monika <monika.hejzner@gmail.com>
2016-11-08 23:25:15 +0100

    new buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 $(function() {
 	var carouselList = $('#carousel ul');
-	setInterval(changeImg, 3000); 
+	var prevImg = $('#prev');
+	var nextImg = $('#next');
+
+	setInterval(changeImg, 5000); 
+
 	function changeImg() {
-		carouselList.animate({'marginLeft':0}, 500, moveFirstSlide());
+		carouselList.animate({'marginLeft':-400}, 500, movePrevSlide);
 	};
-	function moveFirstSlide() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+
+	function movePrevSlide() {
+		var firstItem = carouselList.find("li:first");  // nie wiem co zrobić żeby to się nie powtarzało
+		var lastItem = carouselList.find("li:last");   	// jak wyrzuce to poza funkcję to nie działa
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	};
+
+	function moveNextSlide() {
+		var firstItem = carouselList.find("li:first");  // nie wiem co zrobić żeby to się nie powtarzało
+		var lastItem = carouselList.find("li:last");    // jak wyrzuce to poza funkcję to nie działa
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:0});
+	};
+
+	prevImg.click(function() {
+		movePrevSlide();
+	});
+
+	nextImg.click(function() {
+		moveNextSlide();
+	});
+
 });
 
 
